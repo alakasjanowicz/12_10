72673a0281a1717655fd4c1af6f3eb1cbbe6e57d
Alicja Maria Kasjanowicz <ak68304@student.sgh.waw.pl>
2017-10-12 14:51:09 +0200

    Initial commit

---
+++ src/pl/waw/sgh/Main.java
@@ -0,0 +1,21 @@
+package pl.waw.sgh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello!!!");
+        byte b1;
+        b1=56;
+        System.out.println("b1="+b1);
+
+        float f1=1232141241;
+        double d1=5.5324*10e9;
+        System.out.println("f1="+f1);
+        System.out.println("d1="+d1);
+
+        boolean p =true && false;
+        boolean q = true || false;
+        System.out.println("c="b);
+    }
+}
